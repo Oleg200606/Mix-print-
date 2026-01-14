abb4575a9251378f0e09da2232ef1f29f27240df
Katy248 <petrovanton247@gmail.com>
2026-01-14 16:32:34 +0300

    Added yandex form

---
+++ src/components/CommercialOfferForm.tsx
@@ -0,0 +1,13 @@
+export default function CommercialOfferForm() {
+  return (
+    <>
+      <script src="https://forms.yandex.ru/_static/embed.js"></script>
+      <iframe
+        className="w-full"
+        src="https://forms.yandex.ru/cloud/696790f6eb61460964cffc16?iframe=1"
+        frameBorder="0"
+        name="ya-form-696790f6eb61460964cffc16"
+      ></iframe>
+    </>
+  );
+}
